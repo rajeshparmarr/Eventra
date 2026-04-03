06d8a21e7ec169f1f1c9523e336fa6aeae20e447
RajeshParmarr <rp69576@gmail.com>
2026-04-03 11:04:21 +0530

    Added otp sending logic

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,4 +1,7 @@
 const User = require('../models/User.js')
+const OTP = require('../models/OTP.js')
+const bcrypt = require('bcryptjs')
+const {sendOTPEmail} = require('../utils/email.js')
 
 // Register User
 exports.registerUser = async (req, res) => {
@@ -15,14 +18,18 @@ exports.registerUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     try {
-        const user = new User({ name, email, password: hashedPassword });
-        await user.save();
-        res.status(201).json({
-            message:"User registered successfully"
-        })
+        const user = await User.create({name,email,password,hashedPassword, role:'user', isVerified:false})
 
         const otp = Math.floor(1000000 + Math.random() * 9000000).toString();
         console.log(`Otp for ${email}: ${otp}  `)
+        await OTP.create({ email, otp, action: "account_verification" });
+        await sendOTPEmail(email, otp, 'account_verification')
+
+        res.status(201).json({
+              message: "User registered successfully. Please check your email for OTP to verify your account.",
+              email:user.email
+          });
+
     } catch (error) {
         res.status(400).json({
             error:error.message
