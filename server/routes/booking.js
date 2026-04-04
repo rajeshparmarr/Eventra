6bd1e6e7a83223a151e39d4edbf2d04418d430d9
RajeshParmarr <rp69576@gmail.com>
2026-04-04 18:54:23 +0530

    resolved import issues in booking routes

--- server/routes/booking.js
+++ server/routes/booking.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router()
 const { protect, admin } = require('../middleware/auth.js')
+const {bookEvent,sendBookingOTP,getMyBookings,confirmBooking,cancelBooking} = require('../controller/bookingController.js')
 
 router.post('/', protect, bookEvent);
 router.post('/send-otp', protect, sendBookingOTP);
-router.get('/my', protect, getMyBooking);
+router.get('/my', protect, getMyBookings);
 router.put('/:id/confirm', protect, admin, confirmBooking);
 router.delete('/:id', protect, cancelBooking)
 
